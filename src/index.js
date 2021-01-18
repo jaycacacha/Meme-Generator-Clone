45e12f1b1d637444403e08d47c2f655fb07763a2
Jay Cacacha <75822555+jaycacacha@users.noreply.github.com>
2021-01-18 19:25:11 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import { App } from "./App/App";
 import { BrowserRouter } from "react-router-dom";
-import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <BrowserRouter basename="/meme-generator-clone">
@@ -12,7 +11,3 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
